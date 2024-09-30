d2daf534b1c3ed0f1947dfe6cd66a04b527fcf1c
Shingugitvr000 <Shingugitvr000@gmail.com>
2024-09-30 14:37:31 +0900

    [Feat] Routes 경로 설정

---
+++ Node/Routes/PlayerRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');   //fs 모듈 추가
+const router = express.Router();
+
+//초기 자원 설정
+const initalResources = {
+    metal : 500,
+    crystal : 300,
+    deuterirum : 100,
+}
+
+//플레이어 등록
+//router.post()
+
+//글로벌 플레이어 객체 초기화 
+global.players = {};            //글로벌 객체 초기화 
+
+
+module.exports = router;            //라우터 내보내기 
